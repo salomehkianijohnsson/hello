d276070463603b8c55fbb6642a64dff6e60be74d
Salomeh Kiani Johnsson <salomeh.kiani.johsson@gmail.com>
2016-08-26 21:04:01 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+This is the readme file
+========================
+
+Describe the project so all can download and install.
---
+++ hello.php
@@ -0,0 +1,9 @@
+<html>
+<head>
+    <title>Hello World</title>
+</head>
+<body>
+    <h1>Hello World</h1>
+    <?php echo "My name is Mega"; ?>
+</body>
+</html>
